e466f37f4e4ef5ded453a2ac456d7b8c1db2416e
Shubham Singh <shubhamsingh@Shubhams-MacBook-Pro.local>
2022-03-11 16:53:22 +0530

    second

---
+++ LessionTwo.java
@@ -0,0 +1,9 @@
+public class LessionTwo{
+    public static void main(String[] args) {
+        System.out.println("Shubham");
+        int num1=12;
+        int num2=13;
+        int sum = num1 + num2;
+        System.out.println(sum);
+    }
+}
